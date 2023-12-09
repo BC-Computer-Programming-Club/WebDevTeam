47a916873a289d0992e4068b885c43245314dc1e
Vincent Chau <43220811+vincentczhou@users.noreply.github.com>
2023-12-09 01:11:46 -0800

    Minor Refactoring, Gallery Implementation

---
+++ data/gallery.js
@@ -0,0 +1,20 @@
+const galleryData = [
+  {
+    name: "Bellevue College InfoSec Club",
+    author: "Vincent Chau",
+    authorLink: "https://vincentczhou.github.io",
+    description: "BC InfoSec Club Website",
+    link: "https://www.bcinfosec.me",
+    img: "https://www.bcinfosec.me/_next/image?url=%2Fstatic%2Fimages%2Favatars%2Fdefault.png&w=640&q=75",
+  },
+  {
+    name: "Vincent Chau",
+    author: "Vincent Chau",
+    authorLink: "https://vincentczhou.github.io",
+    description: "Vincent Chau's Portfolio",
+    link: "https://vincentczhou.github.io",
+    img: "https://vincentczhou.github.io/static/images/banner-img.png",
+  },
+];
+
+exports.galleryData = galleryData;
